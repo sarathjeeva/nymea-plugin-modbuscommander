87ba24f12caa875b7aca5c6492f4da1018e92cb8
Michael Zanetti <michael_zanetti@gmx.net>
2019-09-20 17:40:23 +0200

    Update to new plugin api

--- devicepluginmodbuscommander.h
+++ devicepluginmodbuscommander.h
@@ -42,10 +42,10 @@ public:
     explicit DevicePluginModbusCommander();
 
     void init() override;
-    Device::DeviceError discoverDevices(const DeviceClassId &deviceClassId, const ParamList &params) override;
-    Device::DeviceSetupStatus setupDevice(Device *device) override;
+    void discoverDevices(DeviceDiscoveryInfo *info) override;
+    void setupDevice(DeviceSetupInfo *info) override;
     void postSetupDevice(Device *device) override;
-    Device::DeviceError executeAction(Device *device, const Action &action) override;
+    void executeAction(DeviceActionInfo *info) override;
     void deviceRemoved(Device *device) override;
 
 private:
